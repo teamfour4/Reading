3f0d67719339f7867f7ba3a72cc88bb28c1cdbd1
childe <childe@CHILDEEEB4>
2017-06-15 12:04:52 +0800

    图书分类

---
+++ Reading/Controllers/BookClassifyController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reading.Controllers
+{
+    public class BookClassifyController : Controller
+    {
+        // GET: BookClassify
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FantasyBook()//玄幻分类
+        {
+            return View();
+        }
+        public ActionResult EmpriseBook()//武侠分类
+        {
+            return View();
+        }
+        public ActionResult MetropolisBook()//都市分类
+        {
+            return View();
+        }
+        public ActionResult HistoryBook()//历史分类
+        {
+            return View();
+        }
+        public ActionResult GameBook()//游戏分类
+        {
+            return View();
+        }
+        public ActionResult ScifiBook()//科幻分类
+        {
+            return View();
+        }
+    }
+}
