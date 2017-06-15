ff8d40c1769dfbbdcd32bb808334f8853dc86d11
pangyuan <pangyuan@FC11>
2017-06-15 10:55:21 +0800

    py

---
+++ Reading/Controllers/PersonalController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reading.Controllers
+{
+    public class PersonalController : Controller
+    {
+        // GET: Personal
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Personal()
+        {
+            return View();
+        }
+    }
+}
