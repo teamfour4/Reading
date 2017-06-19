ee26bd885825fbe81da57df3da32e0612ecf9015
childe <childe@CHILDEEEB4>
2017-06-19 15:54:41 +0800

    1

--- Reading/Controllers/LoginController.cs
+++ Reading/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reading.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace Reading.Controllers
 {
     public class LoginController : Controller
     {
+
+        private ReadingContext db = new ReadingContext();
         // GET: Login
         public ActionResult Index()
         {
@@ -18,5 +21,44 @@ namespace Reading.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult login(string userId, string password, string returnUrl)
+        {
+
+
+
+            if (!ValidataUser(userId, password))
+            {
+                Session["Exception"] = "用户名或密码错误！";
+                return RedirectToAction("Login", "Login");
+                //FormsAuthentication.SetAuthCookie(Email ,false );
+
+            }
+            if (String.IsNullOrEmpty(returnUrl))
+            {
+                return RedirectToAction("HomePage", "HomePage");
+            }
+            else
+            {
+                return Redirect(returnUrl);
+            }
+            //ModelState.AddModelError("", "您输入的账号密码错误");
+
+        }
+       
+        public bool ValidataUser(string UserId, string Password)
+        {
+            //根据传入的Email和password的值进行检索
+            var users = db.Users.FirstOrDefault(u => u.userId == UserId && u.password == Password);
+            if (users != null)
+            {
+                Session["userId"] = UserId;
+                //Session["Role"] = member.roles.Name;
+
+                return true;
+            }
+
+            return false;
+        }
     }
 }
--- Reading/Controllers/RegisterController.cs
+++ Reading/Controllers/RegisterController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Reading.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,58 @@ namespace Reading.Controllers
 {
     public class RegisterController : Controller
     {
+        private ReadingContext db = new ReadingContext();
         // GET: Member
         public ActionResult Register()
         {
             return View();
         }
-    }
+        [HttpPost]
+        public ActionResult Register(FormCollection fc, User user)
+        {
+            var cnk_user = db.Users.FirstOrDefault(p => p.userId == user.userId);
+            if (cnk_user != null)
+            {
+                ModelState.AddModelError("userId", "该用户名已被注册！");
+                return View();
+            }
+            user.password = fc["password"].ToString().Trim();
+          if(ModelState .IsValid )
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Login","Login");
+            }
+            return View();
+        }
+        public ActionResult modUserPassword()
+        {
+            if(Session ["userId"]==null)
+            {
+                return RedirectToAction("Login","Login");
+            }
+            string userid = Session["userId"].ToString().Trim();
+            var user = db.Users.FirstOrDefault(m=>m.userId ==userid);
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult modUserPassword(FormCollection fc,User user)
+        {
+            user = db.Users.FirstOrDefault(m => m.userId == user.userId);
+            string txtpwd = fc["txtpassword"].ToString().Trim();
+            string txtpwd1 = fc["txtPassword1"].ToString().Trim();
+            if (!txtpwd.Equals(user.password))
+            {
+                Session["Execption"] = "输入的旧密码错误，请从新输入！";
+                return RedirectToAction("modUserPassword", "Register");
+            }
+
+            user .password = txtpwd1;
+            //members.roles = members.roles;
+            db.Entry(user).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("HomePage", "HomePage");
+
+        }
+        }
 }
