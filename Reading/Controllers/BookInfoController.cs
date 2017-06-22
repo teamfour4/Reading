7f578db4724ae778c1beba03907eec45e29f870c
wujintao <wujintao@4D2A>
2017-06-22 18:00:38 +0800

    zy

--- Reading/Controllers/BookInfoController.cs
+++ Reading/Controllers/BookInfoController.cs
@@ -15,14 +15,26 @@ namespace Reading.Controllers
 
         public ActionResult ShowBookInfo()
         {
-            return View();
-        }
+           /* id = 1;*///假设
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
 
-        public ActionResult ReadBook()
-        {
-            return View();
+            /* Chapter  c = db.Chapters.Find(id); *///select *from Chapters where Books_bookId=1 and chaptername=1
+            Chapter c = db.Chapters.Single(a => a.Books .bookId == id&&a.chapterName==1 );
+            //Chapter x=db.Chapters .Where (a => a.Books.bookId == id)
+            //c.Books.bookId =(int) id;
+            //int b = c.Books.bookId;//通过书的id来找章节
+            //db.Chapters.Find();
+            if (c == null)
+            {
+                return HttpNotFound();
+            }
+            return View(c);
         }
-        public ActionResult Index(string SearchString)
+        // GET: Movies/Edit/5
+        public ActionResult ShowBookInfo(int? id)
         {
             //var a=Request.QueryString["SearchString"].ToString();
 
